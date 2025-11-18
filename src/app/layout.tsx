fcc5067bc041bf06f92c26224d2fbc5d7421cfc2
beka009-12 <bekbolnurmsmitov220099@gmail.com>
2025-11-18 17:39:01 +0600

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Provider } from "@/provider/Provider";
+import LayoutClient from "./layout.c";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable}`}
         cz-shortcut-listen="true"
       >
-        <Provider>{children}</Provider>
+        <LayoutClient>{children}</LayoutClient>
       </body>
     </html>
   );
